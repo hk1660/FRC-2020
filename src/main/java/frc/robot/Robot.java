44c05acaf9b5091caf2b7b23653c60635c9b372a
unimportant22 <ajemerenciano3@gmail.com>
2020-02-15 14:52:14 -0500

    Added a command for driving

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.DriveTrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,6 +22,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot
 {
     private Command autonomousCommand;
+    public static DriveTrain driveTrain;
 
     private RobotContainer robotContainer;
 
@@ -34,6 +36,7 @@ public class Robot extends TimedRobot
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         robotContainer = new RobotContainer();
+        driveTrain = new DriveTrain();
     }
 
     /**
---
+++ src/main/java/frc/robot/commands/DriveForwardJoyStick.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Robot;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Have the robot drive mecanum style using the XBOXONE Joystick until interrupted.
+ */
+class DriveForwardJoystick implements Command {
+    Set<Subsystem> dfj = new HashSet<Subsystem>();
+    public DriveForwardJoystick() {
+
+        dfj.add(Robot.driveTrain);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+
+    public  void execute() {
+        //Robot.m_drivetrain.setForwardSpeed(Robot.m_oi.getDriverStick().getRightStickRaw_Y());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+
+    public boolean isFinished() {
+        return false; // Runs until interrupted
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return dfj;
+    }
+
+    // Called once after isFinished returns true
+
+    protected void end() {
+        Robot.driveTrain.setForwardSpeed(0.0);
+    }
+}
